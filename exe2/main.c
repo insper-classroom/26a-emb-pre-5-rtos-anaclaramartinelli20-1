fd00cc25dba318212e8b6539c33583bb3945d0f7
anaclaramartinelli20 <anaclara@AnaClara.localdomain>
2026-04-10 09:10:57 -0300

    exe2.1

--- exe2/main.c
+++ exe2/main.c
@@ -64,22 +64,11 @@ void led_2_task(void *p) {
 }
 
 void btn_2_task(void *p) {
-  gpio_init(BTN_PIN_G);
-  gpio_set_dir(BTN_PIN_G, GPIO_IN);
-  gpio_pull_up(BTN_PIN_G);
-
-  gpio_init(LED_PIN_R);
-  gpio_set_dir(LED_PIN_R, GPIO_IN);
-  gpio_pull_up(LED_PIN_R);
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
 
   while (true) {
-    if (!gpio_get(BTN_PIN_G)) {
-      while (!gpio_get(BTN_PIN_G)) {
-        vTaskDelay(pdMS_TO_TICKS(1));
-      }
-      xSemaphoreGive(xSemaphore_r);
-    }
-
     if (!gpio_get(BTN_PIN_R)) {
       while (!gpio_get(BTN_PIN_R)) {
         vTaskDelay(pdMS_TO_TICKS(1));
@@ -98,8 +87,10 @@ int main() {
   xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
   xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
 
+  xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
+  xTaskCreate(btn_2_task, "BTN_Task 2", 256, NULL, 1, NULL);
+
   vTaskStartScheduler();
 
-  while (true)
-    ;
+  while (true);
 }
