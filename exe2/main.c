4a607a74d854aafc2a614cbe8725f0b69f5b69b4
anaclaramartinelli20 <anaclara@AnaClara.localdomain>
2026-04-10 09:03:49 -0300

    exe2

--- exe2/main.c
+++ exe2/main.c
@@ -32,11 +32,54 @@ void led_1_task(void *p) {
 }
 
 void btn_1_task(void *p) {
-  gpio_init(BTN_PIN_R);
-  gpio_set_dir(BTN_PIN_R, GPIO_IN);
-  gpio_pull_up(BTN_PIN_R);
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
 
   while (true) {
+    if (!gpio_get(BTN_PIN_G)) {
+      while (!gpio_get(BTN_PIN_G)) {
+        vTaskDelay(pdMS_TO_TICKS(1));
+      }
+      xSemaphoreGive(xSemaphore_r);
+    }
+  }
+}
+
+void led_2_task(void *p) {
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+
+  int delay = 250;
+
+  while (true) {
+
+    if (xSemaphoreTake(xSemaphore_r, pdMS_TO_TICKS(500)) == pdTRUE) {
+      gpio_put(LED_PIN_R, 1);
+      vTaskDelay(pdMS_TO_TICKS(delay));
+      gpio_put(LED_PIN_R, 0);
+      vTaskDelay(pdMS_TO_TICKS(delay));
+    }
+  }
+}
+
+void btn_2_task(void *p) {
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
+
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_R, GPIO_IN);
+  gpio_pull_up(LED_PIN_R);
+
+  while (true) {
+    if (!gpio_get(BTN_PIN_G)) {
+      while (!gpio_get(BTN_PIN_G)) {
+        vTaskDelay(pdMS_TO_TICKS(1));
+      }
+      xSemaphoreGive(xSemaphore_r);
+    }
+
     if (!gpio_get(BTN_PIN_R)) {
       while (!gpio_get(BTN_PIN_R)) {
         vTaskDelay(pdMS_TO_TICKS(1));
